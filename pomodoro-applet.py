332086750e03272be1810e66ce388b9d40c78ec4
Dom Binks <dominic.binks@gmail.com>
2024-09-30 13:23:03 +0100

    Add start and clear buttons
    Added start and clear buttons for starting and clearing the timer.

--- pomodoro-applet.py
+++ pomodoro-applet.py
@@ -4,26 +4,48 @@ from gi.repository import Gtk, GLib
 
 class TrayApp:
     def __init__(self):
-        # Create the tray icon
         self.status_icon = Gtk.StatusIcon()
         self.status_icon.set_from_icon_name("dialog-information")
         self.status_icon.connect("activate", self.on_left_click)
         self.status_icon.connect("popup-menu", self.on_right_click)
-        self.count = 0
         self.dialog = None
-        self.dialog_label = None
         self.hours_button = None
         self.minutes_button = None
         self.seconds_button = None
+        self.timer_label = None
+        self.timer = 0
+        self.timer_timeout = None
 
-        GLib.timeout_add(1000, self.increment_counter)
 
-    def increment_counter(self):
-        self.count += 1
-        self.status_icon.set_tooltip_text(str(self.count))
+    def on_start(self, button):
+        self.timer = self.hours_button.get_value() * 360 + \
+                self.minutes_button.get_value() * 60 + \
+                self.seconds_button.get_value()
+        self.timer_label.set_text(str(self.timer))
+
+        if self.timer_timeout:
+            GLib.source_remove(self.timer_timeout)
+
+        self.timer_timeout = GLib.timeout_add(1000, self.decrement_timer)
+
+    def on_clear(self, button):
+        if self.timer:
+            self.timer = 0 
+            self.timer_label.set_text(str(self.timer))
+
+        if self.timer_timeout:
+            GLib.source_remove(self.timer_timeout)
+            self.timer_timeout = None
+
+    def decrement_timer(self):
+        if self.timer == 0:
+            return False
+
+        self.timer -= 1
+        self.status_icon.set_tooltip_text(str(self.timer))
 
         if self.dialog:
-            self.dialog_label.set_text(str(self.count))
+            self.timer_label.set_text(str(self.timer))
 
         return True
 
@@ -34,17 +56,36 @@ class TrayApp:
                 flags = 0
                 )
         box = self.dialog.get_content_area()
+        box.set_border_width(16)
 
-        self.dialog_label = Gtk.Label(label = str(self.count))
-        box.add(self.dialog_label)
-
+        
         self.hours_button = Gtk.SpinButton()
         self.minutes_button = Gtk.SpinButton()
         self.seconds_button = Gtk.SpinButton()
-        box.add(self.hours_button)
-        box.add(self.minutes_button)
-        box.add(self.seconds_button)
 
+        spin_buttons = [self.hours_button, self.minutes_button,
+                        self.seconds_button]
+        spin_button_names = ["Hours", "Minutes", "Seconds"]
+
+        for (button, name) in zip(spin_buttons, spin_button_names):
+            button.set_numeric(True)
+            adjustment = Gtk.Adjustment(upper=59, step_increment=1)
+            button.set_adjustment(adjustment)
+            box.add(Gtk.Label(label = name))
+            box.add(button)
+
+        self.timer_label = Gtk.Label(label = str(self.timer))
+        box.add(self.timer_label)
+
+        start_button = Gtk.Button.new_with_label("Start")
+        start_button.connect("clicked", self.on_start)
+        box.add(start_button)
+
+        clear_button = Gtk.Button.new_with_label("Clear")
+        clear_button.connect("clicked", self.on_clear)
+        box.add(clear_button)
+
+        
         self.dialog.add_button(Gtk.STOCK_OK, Gtk.ResponseType.OK)
     
     def on_dialog_close(self, dialog, response):
@@ -56,10 +97,8 @@ class TrayApp:
             self.create_dialog()
             self.dialog.show_all()
             self.dialog.connect("response", self.on_dialog_close)
-
     
     def on_right_click(self, icon, button, time):
-        # Create a menu
         menu = Gtk.Menu()
         quit_item = Gtk.MenuItem(label="Quit")
         quit_item.connect("activate", Gtk.main_quit)
